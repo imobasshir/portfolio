d6f89d830655a7d2759f65384a0526a271301f90
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-08-04 11:47:00 +0530

    changed ui layout of web

--- lib/screens/home/home_screen_web.dart
+++ lib/screens/home/home_screen_web.dart
@@ -1,4 +1,3 @@
-import 'package:backdrop/backdrop.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio/screens/about/about_web.dart';
